59d0e96060ea0a09c6b85e5d582ea6e4261d228d
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-01-26 23:57:45 +0000

    twinkle + stick figure

--- large-fireworks.py
+++ large-fireworks.py
@@ -13,8 +13,15 @@ pygame.display.set_caption("Fireworks")
 colors = [(255, 0, 0), (255, 165, 0), (255, 255, 0), (0, 255, 0), (0, 0, 255), (75, 0, 130), (238, 130, 238)]
 
 # Create a function to draw a firework
-def draw_firework(x, y, color, radius):
+def draw_firework(x, y, color, radius, trail_color, trail_length):
+    # Draw the trail
+    pygame.draw.circle(screen, trail_color, (x, y), radius, trail_length)
+    # Draw the firework
     pygame.draw.circle(screen, color, (x, y), radius)
+
+# Load the stick figure image
+stick_figure = pygame.image.load("stick_figure.png")
+stick_figure_rect = stick_figure.get_rect()
 
 # Main loop
 done = False
@@ -22,22 +29,27 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-            
+
     # Clear the screen
     screen.fill((0, 0, 0))
-    
+
     # Draw a random firework at a random location
     x = random.randint(50, 650)
     y = random.randint(50, 450)
     color = random.choice(colors)
     radius = random.randint(10, 50)
-    draw_firework(x, y, color, radius)
-    
+    trail_color = (color[0]//2, color[1]//2, color[2]//2)
+    trail_length = random.randint(5, 15)
+    draw_firework(x, y, color, radius, trail_color, trail_length)
+
+    # Draw the stick figure
+    screen.blit(stick_figure, (x-stick_figure_rect.width, y-stick_figure_rect.height-30))
+
     # Update the screen
     pygame.display.flip()
-    
+
     # Delay for a short time
     pygame.time.wait(100)
-    
+
 # Exit pygame
 pygame.quit()
